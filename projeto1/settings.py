14e9b409c7e9613e57d781026cce01f371ba1ff6
Cezar Moreira <cezarcardosomoreira@gmail.com>
2023-01-10 19:47:51 -0300

    project initial

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%9(&ho0q&7b!leoa1-rk%61z=!*)hx9+m$rro(-q$53e6x)!v*'
+SECRET_KEY = 'django-insecure-k7y48o=v&9si-d^q9a-u#-70rs@f#vpka+qm#bz$kzj@lk65qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
