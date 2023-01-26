985368887f3f78dec5b9b5cbaefa1e6f4d82ba87
Cezar Moreira <cezarcardosomoreira@gmail.com>
2023-01-25 22:30:30 -0300

    atualização

--- projeto_agenda/contatos/views.py
+++ projeto_agenda/contatos/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Contato
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+    
+    
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id= contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
